fb3b761a38a6034c65f763a0427e0cc6e49b8830
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-24 00:19:46 +0530

    first commit

---
+++ src/utils/helpers.js
@@ -0,0 +1,18 @@
+export const formatPrice = (number) => {
+  if (number === undefined || number === null) {
+    return 'Price not available'
+  }
+  number=number/100;
+  return number.toLocaleString('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  })
+}
+
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type])
+  if (type === 'colors') {
+    unique = unique.flat()
+  }
+  return ['all', ...new Set(unique)]
+}
